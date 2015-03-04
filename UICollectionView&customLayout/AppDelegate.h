885a2f53a3da63a48bc1f65134d37d6b7d76cfd9
yang <yang@yangzhiqiangdeMacBook-Pro.local>
2015-03-04 20:30:48 +0800

    Initial Commit

---
+++ UICollectionView&customLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView&customLayout
+//
+//  Created by yang on 15/3/4.
+//  Copyright (c) 2015年 yangzhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionView&customLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionView&customLayout
+//
+//  Created by yang on 15/3/4.
+//  Copyright (c) 2015年 yangzhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
